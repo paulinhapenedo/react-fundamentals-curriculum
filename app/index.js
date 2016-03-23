1551cc50d75dec84e6fc5ceea477fb7444463d9c
Paula Penedo <paulinhapenedo@gmail.com>
2016-03-22 21:52:10 -0300

    Remove json fixo

--- app/index.js
+++ app/index.js
@@ -1,15 +1,60 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var HelloWorld = React.createClass({
+// fn(d) = v
+// function(data) = view
+
+var OfferThumbnail = React.createClass({
+	render: function () {
+		return <img src={this.props.thumbnail}  />
+	}
+});
+
+var offerName = React.createClass({
 	render: function () {
 		return (
-			<div> Hello Paula </div>
+			<h1 className="card--product__title">
+				{this.props.children}
+			</h1>
 		)
 	}
 });
 
+var Card = React.createClass({
+	render: function () {
+		return (
+			<div className="bui-card card--product">
+				<OfferThumbnail thumbnail={this.props.products.offers[0].offerThumbnail} />
+				<OfferName />
+			</div>
+		)
+	}
+})
+
 ReactDOM.render(
-	<HelloWorld />,
+	<Card products={PRODUCTS} />,
 	document.getElementById('app')
 );
+
+
+
+
+
+
+
+
+// var HelloWorld = React.createClass({
+// 	render: function () {
+// 		// this.props returns a object with both properties
+// 		// kinda like {name: tyler, anySortOfData: 29}
+// 		console.log(this.props)
+// 		return (
+// 			<div> Hello Paula </div>
+// 		)
+// 	}
+// });
+
+// ReactDOM.render(
+// 	<HelloWorld name="Tyler" anySortOfData={29} />,
+// 	document.getElementById('app')
+// );
